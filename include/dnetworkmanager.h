ea3d9619d5121a59437e8864db97ece105718621
heyuming <heyuming@uniontech.com>
2022-11-08 10:16:51 +0800

    chore: init dtknetworkmanager repo
    Log: 初始化dtknetworkmanager仓库

---
+++ include/dnetworkmanager.h
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#ifndef DNETWORKMANAGER_H
+#define DNETWORKMANAGER_H
+
+#endif  // DNETWORKMANAGER_H
